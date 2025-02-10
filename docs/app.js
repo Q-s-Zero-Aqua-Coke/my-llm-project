e164c82cbe4c001c992329e16a9b489a525ea9b8
LeeWonGyu <bitamin707@naver.com>
2025-02-10 17:18:44 +0900

    server url 수정 및 페이지 수정

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,11 @@ async function main() {
   async function handRecommand(event) {
     event.preventDefault();
 
-    const url = 'http://127.0.0.1:3000';
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner-border');
+    document.querySelector('#box').appendChild(spinner);
+
+    const url = 'https://roomy-certain-perigee.glitch.me';
     const formValues = new FormData(document.querySelector('#recommendForm'));
     const inputText = formValues.get('recommendText');
     const response = await fetch(url, {
@@ -16,7 +20,22 @@ async function main() {
     });
     const responseJson = await response.json();
 
-    document.querySelector('#box').textContent = JSON.stringify(responseJson);
+    spinner.remove();
+
+    const { image, desc } = responseJson;
+    console.log('image :', image);
+    const box = document.querySelector('#box');
+    box.innerHTML = '';
+
+    const imageTag = document.createElement('img');
+    imageTag.classList.add('img-fluid');
+    imageTag.src = image; // image - link
+    console.log('imageTag.src :', imageTag.src);
+    box.appendChild(imageTag);
+
+    const descTag = document.createElement('p');
+    descTag.textContent = desc;
+    box.appendChild(descTag);
   }
 
   document.querySelector('#recommendForm').addEventListener('submit', handRecommand);
